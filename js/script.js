2fe003eff05cd1a7665c2857f74e85153f61fc7e
Aleandro2119 <aleandro.basilico@gmail.com>
2021-12-20 15:38:39 +0100

    Started js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+console.log('JS OK')
+
+//Password generator
+
+/*
+1-Chiedo all'utente il suon nome
+2-Chiedo all'utente il suo cognome
+3-Chiedo all'utente il suo colore preferito
+4-Recupero elemento
+5-Genero la password sulla pagina
+*/
+
+
+
